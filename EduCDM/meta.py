8413e1602565c5668eb35abe2f3f29b4709c9d95
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-03-17 21:17:07 +0800

    [feat] add meta CDM class

---
+++ EduCDM/meta.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+# 2021/3/17 @ tongshiwei
+
+
+def etl(*args, **kwargs) -> ...:  # pragma: no cover
+    """
+    extract - transform - load
+    """
+    pass
+
+
+def train(*args, **kwargs) -> ...:  # pragma: no cover
+    pass
+
+
+def evaluate(*args, **kwargs) -> ...:  # pragma: no cover
+    pass
+
+
+class CDM(object):
+    def __init__(self, *args, **kwargs) -> ...:
+        pass
+
+    def train(self, *args, **kwargs) -> ...:
+        raise NotImplementedError
+
+    def eval(self, *args, **kwargs) -> ...:
+        raise NotImplementedError
+
+    def save(self, *args, **kwargs) -> ...:
+        raise NotImplementedError
+
+    def load(self, *args, **kwargs) -> ...:
+        raise NotImplementedError
