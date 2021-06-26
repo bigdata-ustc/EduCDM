bbe3ea7f30e972b33ac61652f6d2055a67b7cee6
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-06-26 19:57:32 +0800

    [refactor] use as_list from longling

--- EduCDM/IRR/sampler.py
+++ EduCDM/IRR/sampler.py
@@ -5,40 +5,9 @@ from collections import Iterable
 
 import pandas as pd
 from tqdm import tqdm
+from longling import as_list
 
 __all__ = ["UITripletPairSampler"]
-
-
-def as_list(obj) -> list:
-    r"""A utility function that converts the argument to a list
-    if it is not already.
-
-    Parameters
-    ----------
-    obj : object
-        argument to be converted to a list
-
-    Returns
-    -------
-    list_obj: list
-        If `obj` is a list or tuple, return it. Otherwise,
-        return `[obj]` as a single-element list.
-
-    Examples
-    --------
-    >>> as_list(1)
-    [1]
-    >>> as_list([1])
-    [1]
-    >>> as_list((1, 2))
-    [1, 2]
-    """
-    if isinstance(obj, list):
-        return obj
-    elif isinstance(obj, tuple):
-        return list(obj)
-    else:
-        return [obj]
 
 
 class Sampler(object):
