139139c3a46d82e7bbda6190619dfd8627c8ace1
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-06-27 11:57:34 +0800

    [tests] for STE

--- tests/dina/gd/test_gddina.py
+++ tests/dina/gd/test_gddina.py
@@ -1,12 +1,14 @@
 # coding: utf-8
 # 2021/4/23 @ tongshiwei
 
+import pytest
 from EduCDM import GDDINA
 
 
-def test_train(data, conf, tmp_path):
+@pytest.mark.parametrize("ste", [True, False])
+def test_train(data, conf, tmp_path, ste):
     user_num, item_num, knowledge_num = conf
-    cdm = GDDINA(user_num, item_num, knowledge_num)
+    cdm = GDDINA(user_num, item_num, knowledge_num, ste=ste)
     cdm.train(data, test_data=data, epoch=2)
     filepath = tmp_path / "dina.params"
     cdm.save(filepath)
