80d6fc8dd4a0b24127251a347c3679f0645f8f8a
tswsxk <tongsw@mail.ustc.edu.cn>
2021-10-23 16:23:47 +0800

    Update MIRT.py

--- EduCDM/MIRT/MIRT.py
+++ EduCDM/MIRT/MIRT.py
@@ -60,8 +60,7 @@ class MIRTNet(nn.Module):
         else:
             a = F.softplus(a)
         b = torch.squeeze(self.b(item), dim=-1)
-        if torch.max(theta != theta) or torch.max(a != a) or torch.max(b != b):
-            # pragma: no cover
+        if torch.max(theta != theta) or torch.max(a != a) or torch.max(b != b):  # pragma: no cover
             raise ValueError('ValueError:theta,a,b may contains nan!  The a_range is too large.')
         return self.irf(theta, a, b, **self.irf_kwargs)
 
