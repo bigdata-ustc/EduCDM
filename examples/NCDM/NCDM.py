07956855cc8ac13634c4fbc484b85a0d412d6270
LegionKing <pursuerwf@gmail.com>
2023-11-17 21:36:26 +0800

    update example files of NCDM

--- examples/NCDM/NCDM.py
+++ examples/NCDM/NCDM.py
@@ -9,7 +9,9 @@ import numpy as np
 from EduData import get_data
 
 
-# get_data("cdbd-a0910", "../../data")
+# get_data("cdbd-a0910", "../../data")   # Download dataset "cdbd-a0910"
+
+# load data and transform it to the required format
 train_data = pd.read_csv("../../data/a0910/train.csv")
 valid_data = pd.read_csv("../../data/a0910/valid.csv")
 test_data = pd.read_csv("../../data/a0910/test.csv")
@@ -20,20 +22,25 @@ for i, s in df_item.iterrows():
     knowledge_set.update(knowledge_codes)
     item_set.add(item_id)
 userIds = train_data['user_id'].unique()
-meta_data = {'userId': userIds, 'itemId': list(item_set), 'skill': list(knowledge_set)}
+meta_data = {'userId': list(userIds), 'itemId': list(item_set), 'skill': list(knowledge_set)}
 train_data = (pd.merge(train_data, df_item, how='left', on='item_id')
               .rename(columns={'user_id': 'userId', 'item_id': 'itemId', 'knowledge_code': 'skill', 'score': 'response'}))
 valid_data = pd.merge(valid_data, df_item, how='left', on='item_id').rename(columns={'user_id': 'userId', 'item_id': 'itemId', 'knowledge_code': 'skill', 'score': 'response'})
 test_data = pd.merge(test_data, df_item, how='left', on='item_id').rename(columns={'user_id': 'userId', 'item_id': 'itemId', 'knowledge_code': 'skill', 'score': 'response'})
 
+# model training
 batch_size = 32
-
 logging.getLogger().setLevel(logging.INFO)
 cdm = NCDM(meta_data)
 cdm.fit(train_data, epoch=1, val_data=valid_data, device="cuda")
+
+# predict using the trained model
 print(cdm.predict(test_data))
+
+# save model
 cdm.save("ncdm.snapshot")
 
+# load model and evaluate it on the test set
 cdm.load("ncdm.snapshot")
 auc, accuracy = cdm.eval(test_data)
 print("auc: %.6f, accuracy: %.6f" % (auc, accuracy))
