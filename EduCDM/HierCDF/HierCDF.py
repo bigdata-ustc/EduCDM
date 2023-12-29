7807e60f157a632274018cb71320905c2f57b046
CSLiJT <satosasara@mail.ustc.edu.cn>
2023-12-29 20:24:57 +0800

    add pytest for hiercdf

--- EduCDM/HierCDF/HierCDF.py
+++ EduCDM/HierCDF/HierCDF.py
@@ -16,8 +16,8 @@ from tqdm import tqdm
 from typing import Tuple
 
 # TODO: delete this line in deployment
-# import sys
-# sys.path.append('..')
+import sys
+sys.path.append('..')
 
 from EduCDM import CDM, re_index
 
@@ -535,15 +535,33 @@ class HierCDLoss(nn.Module):
 
 
 def _test():
-    train_data = pd.read_csv('../tests/data/train_0.8_0.2.csv')
-    know_graph = pd.read_csv('../tests/data/hier.csv')
-    q_matrix = np.loadtxt('../tests/data/Q_matrix.txt')
-    train_data['skill'] = None
+    # train_data = pd.read_csv('../tests/data/train_0.8_0.2.csv')
+    # know_graph = pd.read_csv('../tests/data/hier.csv')
+    # q_matrix = np.loadtxt('../tests/data/Q_matrix.txt')
+    train_data = pd.DataFrame({
+        'userId': [
+            '001', '001', '001', '001', '002', '002',
+            '002', '002', '003', '003', '003', '003'],
+        'itemId': [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3],
+        'response': [0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1]
+    })
+    know_graph = pd.DataFrame({
+        'source': [0, 0, 1],
+        'target': [1, 2, 3]
+    })
+    q_matrix = np.array([
+        [1, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 1, 1],
+        [1, 0, 0, 1]
+    ])
+
+    train_data['skill'] = 0
     for id in range(train_data.shape[0]):
         item_id = train_data.loc[id, 'itemId']
-        train_data.loc[id, 'skill'] = np.where(
-            q_matrix[item_id] > 0)[0]
-        train_data.loc[id, 'skill'] = str(train_data.loc[id, 'skill'])
+        concepts = np.where(
+            q_matrix[item_id] > 0)[0].tolist()
+        train_data.loc[id, 'skill'] = str(concepts)
     meta_data = {'userId': [], 'itemId': [], 'skill': []}
     meta_data['userId'] = train_data['userId'].unique().tolist()
     meta_data['itemId'] = train_data['itemId'].unique().tolist()
@@ -551,8 +569,17 @@ def _test():
 
     hiercdm = HierCDF(meta_data, know_graph, hidd_dim=32)
     hiercdm.fit(
-        train_data.loc[:1000, :],
-        val_data=train_data.loc[1000:1100, :], epoch=3)
+        train_data,
+        val_data=train_data,
+        batch_size=1, epoch=3, lr=0.01)
+    hiercdm.save('./hiercdf.pt')
+    new_hiercdm = HierCDF(meta_data, know_graph, hidd_dim=32)
+    new_hiercdm.load('./hiercdf.pt')
+    new_hiercdm.fit(
+        train_data,
+        val_data=train_data,
+        batch_size=1, epoch=1, lr=0.01)
+    new_hiercdm.eval(train_data)
 
 
 if __name__ == '__main__':
